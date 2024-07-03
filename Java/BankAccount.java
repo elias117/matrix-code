8d289010893250b6cd196e1e6380a634fd8c691f
Elias Zoghaib <elias.zoghaib@randstad.ca>
2024-07-03 11:23:44 -0300

    Added some more examples and changes to bank account example

--- Java/BankAccount.java
+++ Java/BankAccount.java
@@ -2,13 +2,14 @@ public class BankAccount {
     private String name;
     private double balance;
 
+    // constructor method to create a bank account with a name and balance
     public BankAccount(String name, double balance) {
         this.name = name;
         this.balance = balance;
     }
 
-    public BankAccount(String PassedInName) {
-        this.name = PassedInName;
+    public BankAccount(String name) {
+        this.name = name;
         this.balance = 0.0;
     }
 
@@ -17,14 +18,14 @@ public class BankAccount {
     }
 
     public void Withdraw(double amount) {
-        if (amount <= balance) {
-            balance = balance - amount;
-        } else {
+        if (amount > balance) {
             System.out.println("Insufficient Balance");
+            return;
         }
+        balance = balance - amount;
     }
 
-    public void Display(){
+    public void Display() {
         System.out.println("Name: " + name);
         System.out.println("Balance: " + balance);
     }
--- Java/BankAccountDriver.java
+++ Java/BankAccountDriver.java
@@ -1,16 +1,33 @@
 public class BankAccountDriver {
     public static void main(String[] args) {
-        BankAccount accountForElias = new BankAccount("Elias", 1000000.00);
+        BankAccount accountForElias = new BankAccount("Elias", 1000.00);
+        BankAccount accountForNate = new BankAccount("Nate");
 
-        accountForElias.Display();
+        accountForNate.Display();
+
+        accountForNate.Deposit(10000);
+
+        System.out.println("New balance after depositing $10,000");
+        accountForNate.Display();
+
+        accountForNate.Withdraw(5000);
+
+        System.out.println("New balance after withdrawing $5,000");
+        accountForNate.Display();
+
+        accountForNate = new BankAccount("Elias");
+        accountForNate.Display();
+
+        String name = "Nate";
+        String name2 = new String("Elias");
 
         // accountForElias.Withdraw(10000);
 
         // accountForElias.Display();
 
-        BankAccount accountForMaggi = new BankAccount("Maggi", 100000000.00);
+        // BankAccount accountForMaggi = new BankAccount("Maggi", 100000000.00);
 
-        BankAccount accountForNate = new BankAccount("Nate");
+        // BankAccount accountForNate = new BankAccount("Nate");
 
     }
 }
