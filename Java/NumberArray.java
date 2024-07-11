1c6b54d5dd7af9911c2b25149e8a78750a090b4f
Elias Zoghaib <elias.zoghaib@randstad.ca>
2024-07-11 11:24:29 -0300

    Added some comments

--- Java/NumberArray.java
+++ Java/NumberArray.java
@@ -1,11 +1,17 @@
 public class NumberArray {
     public int[] getArray() {
         int[] numbers = { 1, 2, 3, 4, 5 };
+        // I can reference static methods and static variables here
+        // This is because static methods and static variables belong to the class
+        // int[] staticNumbers = getArrayStatic();
         return numbers;
     }
 
     public static int[] getArrayStatic() {
         int[] numbers = { 1, 2, 3, 4, 5 };
+        // I can't reference non-static methods and non-static variables here
+        // This is because non-static methods and non-static variables belong to the object
+        // int[] nonStaticNumbers = getArray();
         return numbers;
     }
 }
