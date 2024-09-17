7fce7f3c3fbd70b74512a7827337307d490c7041
Elias Zoghaib <elias.zoghaib@randstad.ca>
2024-09-17 10:46:55 -0300

    Added dom example

---
+++ js/background.js
@@ -0,0 +1,8 @@
+const changeBackgroundColorRed = () =>{
+    document.body.style.backgroundColor = "red";
+}
+
+function changeBackgroundColorBlue() {
+    document.getElementById("example").style.backgroundColor = "blue";
+    document.getElementById("example").style.color = "white";
+}
